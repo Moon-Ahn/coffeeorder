3f7f0cd3ae6b600df2e884932dd128d32f78e6eb
munan <kicest0731@gmail.com>
2023-05-26 15:31:25 +0900

    first commit

--- main.py
+++ main.py
@@ -11,12 +11,32 @@ def home():
         # Get the coffee order details from the form
         name = request.form['name']
         coffee_type = request.form['coffee_type']
-        size = request.form['size']
+        temp = request.form['temp']
 
         # Add the order to the list
-        coffee_orders.append({'name': name, 'type': coffee_type, 'size': size})
+        coffee_orders.append({'name': name, 'type': coffee_type, 'temp': temp})
 
     return render_template('index.html', orders=coffee_orders)
+
+
+@app.route('/beverage_data')
+def beverage_data():
+    # Create a dictionary to store the drink quantities
+    drink_quantities = {}
+
+    # Iterate over the coffee orders
+    for order in coffee_orders:
+        drink_type = order['type']
+        temp = order['temp']
+        key = (drink_type, temp)
+
+        # Increment the drink quantity if the key exists, otherwise initialize it to 1
+        if key in drink_quantities:
+            drink_quantities[key] += 1
+        else:
+            drink_quantities[key] = 1
+
+    return render_template('beverage_data.html', drink_quantities=drink_quantities)
 
 if __name__ == '__main__':
     app.run(host='192.168.1.46',debug=True)
